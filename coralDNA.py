13cbdb588585f0ae77da84280eabe94901ba72f7
Talhah Mohammed <talhahum@gmail.com>
2018-07-03 15:33:35 +0300

    new update

---
+++ coralDNA.py
@@ -0,0 +1,21 @@
+def ncheck(n):
+    if n < 0:
+        return "The number must be positive."
+
+def factorial(n):
+    num = 1
+    while n >= 1:
+        num = num * n
+        n = n - 1
+    return num
+
+
+from math import sqrt
+def fibonacci(n):
+    else:
+        return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
+
+
+ncheck()
+print(factorial(300))
+print(fibonacci(-1))
